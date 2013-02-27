e7cc665275ec435c98f86f82a1399bc6c4115e84
Aitor <aitor.cardona.l@gmail.com>
2013-02-27 10:31:19 +0000

    NumberCalculator class have been created with all the posible param combinations

---
+++ src/Caculator/operations/numberCalculator.java
@@ -0,0 +1,58 @@
+
+package Caculator.operations;
+
+import Caculator.Calculator;
+
+public class numberCalculator extends Calculator{
+    
+   
+    public int addBinaryOperator (int param0, int param1){
+        return param0+param1;
+    }
+    public double addBinaryOperator (double param0, int param1){
+        return param0+param1;
+    }
+    public double addBinaryOperator (int param0, double param1){
+        return param0+param1;
+    }
+    public double addBinaryOperator (double param0, double param1){
+        return param0+param1;
+    }
+ 
+    public int minusBinaryOperator (int param0, int param1){
+        return param0-param1;
+    }
+    public double minusBinaryOperator (double param0, int param1){
+        return  param0-param1;
+    }
+    public double minusBinaryOperator (int param0, double param1){
+        return  param0-param1;
+    }
+    public double minusBinaryOperator (double param0, double param1){
+        return  param0-param1;
+    }
+    public int MultBinaryOperator (int param0, int param1){
+        return  param0*param1;
+    }
+    public double MultBinaryOperator (double param0, int param1){
+        return  param0*param1;
+    }
+    public double MultBinaryOperator (int param0, double param1){
+        return param0*param1;
+    }
+    public double MultBinaryOperator (double param0, double param1){
+        return  param0*param1;
+    }
+    public int DivisionBinaryOperator (int param0, int param1){
+        return  param0/param1;
+    }
+    public double DivisionBinaryOperator (double param0, int param1){
+        return  param0-param1;
+    }
+    public double DivisionBinaryOperator (int param0, double param1){
+        return param0-param1;
+    }
+    public double DivisionBinaryOperator (double param0, double param1){
+        return param0-param1;
+    }
+}
