2f1602eb435f7f526f0ddfd64ae126d9d78758e2
Aitor <aitor.cardona.l@gmail.com>
2013-02-28 11:32:28 +0000

    A new step in our architecture has been reached, we identify the necesity of work an type abstract class. Abstract class has been created. New work package TODO.

---
+++ src/sintaxtree/evaluator/Type.java
@@ -0,0 +1,15 @@
+
+package sintaxtree.evaluator;
+
+public abstract class Type<T> {
+    private T value;
+
+    public Type(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+    
+}
